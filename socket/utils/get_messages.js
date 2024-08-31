da9b28baa707813c496a3b13a1e0ea5a4694eff2
Giovanni Borgogno <101715111+giovaborgogno@users.noreply.github.com>
2024-08-31 20:03:43 -0300

    Revert upgrade deploy action version (#33)
    * Revert "fix env variables in pipeline (#32)"
    
    This reverts commit 0cb7449c36cca9f6919d2aaec0739a1c90954c18.
    
    * Revert "upgrade container-apps-deploy-action to v2 (#31)"
    
    This reverts commit bc729dc9e6237aef2d6ddeffc50f0267fb446733.

--- socket/utils/get_messages.js
+++ socket/utils/get_messages.js
@@ -2,9 +2,12 @@ const { getMessages } = require("../db/client");
 
 async function get_messages() {
     const messagesRoom1 = await getMessages('1');
+
     const messages = {
         '1': messagesRoom1,
     }
+
     return messages;
 }
-module.exports = get_messages;
+  module.exports = get_messages;
+  
