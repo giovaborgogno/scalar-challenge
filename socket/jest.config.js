8551240114ea2d2057cc8fe53c62813acd76e981
Giovanni Borgogno <giovaborgogno@gmail.com>
2024-11-06 19:39:51 -0300

    fix collect coverage from

--- socket/jest.config.js
+++ socket/jest.config.js
@@ -8,5 +8,11 @@ module.exports = {
             titleTemplate: "{title}", // outputname
             outputName: "TEST-socket.xml"
         }]
-    ]
+    ],
+    collectCoverageFrom: [
+        "**/*.{js,jsx}", // Incluye todos los archivos .js y .jsx en el directorio actual
+        "!**/node_modules/**", // Excluye node_modules
+        "!**/*.test.{js,jsx}", // Excluye archivos de prueba
+        "!**/coverage/**", // Excluye el directorio de cobertura si ya existe
+    ],
 };
